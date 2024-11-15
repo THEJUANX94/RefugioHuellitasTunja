f099b50c688e23399fc5cbee7ba7ac4f1ff51900
THEJUANX94 <sebastianmn03@hotmail.com>
2024-11-15 18:02:13 -0500

    implementacion guardar imagenes

--- Server/src/Controllers/petController.js
+++ Server/src/Controllers/petController.js
@@ -1,4 +1,14 @@
 const { pool } = require('../database')
+const { CLOUDINARY_URL } = require('../config');
+const { uploadForm } = require('./FormsController');
+const cloudinary = require('cloudinary').v2;
+const multer = require('multer');
+
+cloudinary.config({
+    cloud_name: 'dg639pvia',
+    api_key: '488141991613393',
+    api_secret: 'LfPK2HfMGfhRxH1TuqSl0tAHB4k'
+});
 
 const createPet = async (req, res) => {
     const { idspecies, name, birthday, race, details } = req.body;
@@ -53,13 +63,37 @@ const deletePet = async (req, res) => {
     });
 }
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/'); // Temporary folder to store files before upload
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + '-' + file.originalname); // Unique filename
+    }
+  });
+  
+const uploadImages = multer({ storage });
 
+const newImage = async (req, res, link) => {
+    const idpet = req.params.idpet;
+    pool.query(`INSERT INTO images (idpet, linkimage) VALUES ('${idpet}', '${link}')`)
+};
 
+const searchImages = async(req, res) => {
+    const idpet = req.params.idpet;
+    pool.query(`SELECT linkimage FROM images WHERE idpet = '${idpet}'`, (err, results) => {
+        if (err) return res.status(404).json({ message: 'Error, no se pudo encontrar la mascota indicada.' });
+        res.status(200).json(results.rows);
+    });
+}
 
 module.exports = {
     createPet,
     getPets,
     getPetsById,
     updatePet,
-    deletePet
+    deletePet,
+    uploadImages,
+    newImage,
+    searchImages
 }
--- Server/src/Routes/petRoutes.js
+++ Server/src/Routes/petRoutes.js
@@ -1,12 +1,40 @@
 // backend/routes/userRoutes.js
 const express = require('express');
-const { createPet, getPets, getPetsById, updatePet, deletePet } = require('../Controllers/petController.js');
+const { createPet, getPets, getPetsById, updatePet, deletePet, uploadImages, newImage, searchImages } = require('../Controllers/petController.js');
 const router = express.Router();
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
 
 router.post('/pets', createPet);
 router.get('/pets', getPets);
 router.get('/pets/:idpet', getPetsById);
 router.put('/pets/:idpet', updatePet);
 router.delete('/pets/:idpet', deletePet);
+router.get('/images/:idpet', searchImages);
+router.post('/upload/:idpet', uploadImages.single('image'), async (req, res) => {
+    try {
+      // Upload the file to Cloudinary using the uploaded file path
+      const result = await cloudinary.uploader.upload(req.file.path, {
+        folder: 'pets', // Folder in Cloudinary to store images
+      });
+      newImage(req, res, result.secure_url);
+      // Respond with the secure URL of the uploaded image
+      res.json({ secure_url: result.secure_url });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error al subir la imagen' });
+    } finally {
+      if (req.file?.path) {
+        fs.unlink(req.file.path, (err) => {
+          if (err) {
+            console.error('Error eliminando el archivo:', err);
+          } else {
+            console.log('Archivo eliminado exitosamente:', req.file.path);
+          }
+        });
+      }
+    }
+  });
+
 
 module.exports = router;
